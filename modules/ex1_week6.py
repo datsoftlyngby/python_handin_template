5a7efb3c5e6ee5b37c18f1369f577119ef416fea
= <cph-cw109@cphbusiness.dk>
2021-03-16 09:49:51 +0100

    week6

---
+++ modules/ex1_week6.py
@@ -0,0 +1,49 @@
+from typing import Text
+import requests
+from concurrent.futures import ThreadPoolExecutor, as_completed
+
+
+class TextComparer:
+    def __init__(self, url_list=[]):
+        self.url_list = url_list
+
+    def download(self, url, filename):
+        try:
+            r = requests.get(url)
+            with open(filename, "a") as file:
+                file.write(r.text)
+        except:
+            raise Exception("Given url not found")
+
+    def multi_download(self):
+        with ThreadPoolExecutor(max_workers=10) as ex:
+            for url in self.url_list:
+                ex.submit(self.download(url, url.split(".")[1]))
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if len(self.url_list) == 0:
+            raise StopIteration
+        return self.url_list.pop()
+
+    def urllist_generator(self):
+        while len(self.url_list) != 0:
+            yield self.url_list.pop()
+
+    def avg_vowels(self, text):
+        all_vowels = "aeiouyæøå"
+        vowels_count = 0
+        all_words = text.split(" ")
+        for word in all_words:
+            for letter in word:
+                if letter in all_vowels:
+                    vowels_count += 1
+        return vowels_count / len(all_words)
+
+  
+
+#tc = TextComparer(["https://www.facebook.com/", "https://www.google.com/"])
+
+#print(tc.avg_vowels("hej med dig"))
---
+++ modules/ex2_week6.py
@@ -0,0 +1,5 @@
+from modules import ex1_week6 as ex6
+
+ex6.multi_download(["https://www.gutenberg.org/files/1232/1232-h/1232-h.htm"])
+
+print(ex6.avg_vowels(["https://www.gutenberg.org/files/1232/1232-h/1232-h.htm"]))
